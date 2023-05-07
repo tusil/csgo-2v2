a01f3e20dc93471f27b38d0499485f9aff17210b
Štěpán Ulver <stepan@youngmedia.cz>
2023-05-07 22:25:36 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# https://github.com/crazy-max/csgo-server-launcher/blob/master/doc/installation.md
+sudo curl -sSLk https://github.com/crazy-max/csgo-server-launcher/releases/download/v1.16.0/install.sh | sudo bash
+
